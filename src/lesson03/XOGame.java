79901f5618d9dbc1047b4d2185f7c3bdf23d1ac7
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2020-12-24 15:42:24 +0300

    Переделан метод проверки победы, а также добавлен алгоритм определения ситуации, когда до победы остаётся один ход.

--- src/lesson03/XOGame.java
+++ src/lesson03/XOGame.java
@@ -36,7 +36,7 @@ public class XOGame {
     public static void initMap(){
         mapSizeX = 5;
         mapSizeY = 5;
-        winLength = 4;
+        winLength = 3;
         map = new char[mapSizeY][mapSizeX];
 
         for (int y = 0; y < mapSizeY; y++) {
@@ -76,12 +76,20 @@ public class XOGame {
         int x;
         int y;
 
-        do {
-            x = RANDOM.nextInt(mapSizeX);
-            y = RANDOM.nextInt(mapSizeY);
-        } while (!isEmptyCell(y, x));
+        // Перед тем как сделать ход случайным образом, компьютер проверяет два условия:
+        // 1. Компьютеру остался один ход до победы
+        // 2. Игроку остался один ход до победы
+        // Если метод checkIsAboutToWin() найдёт такую ситуацию, он сделает этот ход и вернёт true.
+        // Если же ни для компьютера, ни для человека такой ситуации не найдено,
+        // только тогда нужно делать ход случайным образом.
+        if (!checkIsAboutToWin(PC_DOT) && !checkIsAboutToWin(HUMAN_DOT)){
+            do {
+                x = RANDOM.nextInt(mapSizeX);
+                y = RANDOM.nextInt(mapSizeY);
+            } while (!isEmptyCell(y, x));
 
-        map[y][x] = PC_DOT;
+            map[y][x] = PC_DOT;
+        }
     }
 
     /**
@@ -199,6 +207,106 @@ public class XOGame {
     }
 
     /**
+     * Проверяет, не возникла ли такая ситуация, когда до победы одной из сторон остался один ход.
+     * Если такая ситуация возникла, ставит в пустую клетку знак компьютера -
+     * нолик (чтобы победить или блокировать победу игрока)
+     * @param inboxChar Знак игрока или компьютера
+     * @return Истина, если программа нашла такую ситуацию и сделала ход, иначе - ложь
+     */
+    public static boolean checkIsAboutToWin(char inboxChar){
+        for (int y = 0; y < mapSizeY; y++) {
+            for (int x = 0; x < mapSizeX; x++) {
+                if (checkIsAboutToWinForCell(y, x, inboxChar)){
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Проверяет, не начинается ли из этой клетки комбинация, в которой остался один ход до победы.
+     * Если такая комбинация найдена, программа делает этот ход, чтобы победить или блокировать победу игрока.
+     * @param y Координата Y
+     * @param x Координата X
+     * @return Истина, если программа нашла такую комбинацию и сделала ход, иначе - ложь
+     */
+    public static boolean checkIsAboutToWinForCell(int y, int x, char inboxChar){
+        int length = 0;
+
+        // Двигаемся по горизонтали
+        // Конечная позиция - ширина поля минус один, потому что нам
+        // обязательно нужна следующая клетка, и она должна быть пустой
+        for (int i = x; i < mapSizeX - 1; i++) {
+            if (map[y][i] == inboxChar){
+                length++;
+            } else {
+                break;
+            }
+
+            // После каждой итерации проверяем условия: длина ряда должна быть на единицу меньше победной длины,
+            // а следующая клетка должна быть пустой
+            // Если эти условия выполняются, значит, компьютеру или игроку до победы остался один ход.
+            // Вне зависимости от того, чей это ход (компьютера или игрока) ставим в следующую клетку
+            // знак компьютера, так как в первом случае компьютер победит, а во втором - блокирует
+            // победу пользователя. После того, как ход сделан, возвращаем true.
+            if (length == winLength - 1 && map[y][i + 1] == EMPTY_DOT){
+                map[y][i + 1] = PC_DOT;
+                return true;
+            }
+        }
+
+        // Двигаемся по вертикали
+        length = 0;
+        for (int i = y; i < mapSizeY - 1; i++) {
+            if (map[i][x] == inboxChar){
+                length++;
+            } else {
+                break;
+            }
+
+            if (length == winLength - 1 && map[i + 1][x] == EMPTY_DOT){
+                map[i + 1][x] = PC_DOT;
+                return true;
+            }
+        }
+
+        // Двигаемся по главной диагонали (вправо и вниз). В этом случае увеличиваем значения обоих итераторов на 1
+        length = 0;
+        for (int i = y, j = x; i < mapSizeY - 1 && j < mapSizeX - 1; i++, j++) {
+            if (map[i][j] == inboxChar){
+                length++;
+            } else {
+                break;
+            }
+
+            if (length == winLength - 1 && map[i + 1][j + 1] == EMPTY_DOT){
+                map[i + 1][j + 1] = PC_DOT;
+                return true;
+            }
+        }
+
+        // Двигаемся по второстепенной диагонали (влево и вниз). В этом случае итератор i, который идёт по строкам, увеличивается,
+        // а итератор j, который идёт по столбцам, уменьшается
+        length = 0;
+        for (int i = y, j = mapSizeX - 1; i < mapSizeY - 1 && j > 0; i++, j--) {
+            if (map[i][j] == inboxChar){
+                length++;
+            } else {
+                break;
+            }
+
+            if (length == winLength  && map[i + 1][j - 1] == EMPTY_DOT){
+                map[i + 1][j - 1] = PC_DOT;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
      * Проверяет, не заполнилось ли игровое поле (ничья)
      * @return Истина, если игровое поле заполнилось, иначе ложь
      */
@@ -235,7 +343,7 @@ public class XOGame {
             aiTurn();
             printMap();
             if (checkWin(PC_DOT)){
-                System.out.println("Human win!!!");
+                System.out.println("Computer win!!!");
                 break;
             }
             if (isMapFull()){
