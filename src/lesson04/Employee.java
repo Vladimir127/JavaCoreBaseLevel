7887edfb8468008af97f4c1d5b094baa2a8bb7b4
Vladimir Stepanov <vladimir-stepanov-home@yandex.ru>
2020-12-24 17:12:40 +0300

    Выполнено домашнее задание к уроку 4.

--- src/lesson04/Employee.java
+++ src/lesson04/Employee.java
@@ -1,2 +1,95 @@
-package lesson04;public class Employee {
+package lesson04;
+
+/** Сотрудник */
+public class Employee {
+    /**
+     * Количество уже существующих сотрудников - статическое поле,
+     * необходимое для генерации уникального порядкового номера
+     * */
+    private static int countEmployees = 0;
+
+    /** Уникальный порядковый номер сотрудника */
+    private int id;
+
+    /** ФИО */
+    private String fullName;
+
+    /** Должность */
+    private String position;
+
+    /** Телефон */
+    private String phoneNumber;
+
+    /** Зарплата */
+    private int salary;
+
+    /** Возраст */
+    private int age;
+
+    /**
+     * Инициализирует экземпляр класса
+     * @param fullName ФИО
+     * @param position Должность
+     * @param phoneNumber Телефон
+     * @param salary Зарплата
+     * @param age Возраст
+     */
+    public Employee(String fullName, String position, String phoneNumber, int salary, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+        this.age = age;
+
+        // Уникальный порядковый номер представляет собой количество уже существующих сотрудников, увеличенное на 1
+        this.id = ++countEmployees;
+    }
+
+    /**
+     * Возвращает ФИО
+     * @return ФИО
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Возвращает должность
+     * @return Должность
+     */
+    public String getPosition() {
+        return position;
+    }
+
+    /**
+     * Возвращает телефон
+     * @return Телефон
+     */
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * Возвращает зарплату
+     * @return Зарплата
+     */
+    public int getSalary() {
+        return salary;
+    }
+
+    /**
+     * Возвращает возраст
+     * @return Возраст
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Повышает зарплату сотрудника на указанную сумму
+     * @param sum Сумма повышения
+     */
+    public void raiseSalary(int sum) {
+        salary += sum;
+    }
 }
--- src/lesson04/Main.java
+++ src/lesson04/Main.java
@@ -1,2 +1,43 @@
-package lesson04;public class Main {
+package lesson04;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Создаём первого сотрудника.");
+        Employee employee1 = new Employee("Королёв Алексей Иванович", "директор", "8-941-325-69-78", 60000, 36);
+        System.out.println("ФИО: " + employee1.getFullName() + ", должность: " + employee1.getPosition());
+        System.out.println();
+
+        System.out.println("Создаём массив сотрудников.");
+        Employee[] employees = {
+                employee1,
+                new Employee("Петрова Зинаида Фёдоровна", "бухгалтер", "8-962-782-14-56", 35000, 64),
+                new Employee("Степанов Владимир Владимирович", "программист", "8-961-168-02-27", 50000, 29),
+                new Employee("Колмогоров Егор Андреевич", "тестировщик", "8-935-781-65-48", 25000, 22),
+                new Employee("Алексеев Андрей Александрович", "Главный инженер", "8-967-832-47-14", 55000, 45),
+        };
+        System.out.println("Сотрудники старше 40 лет: ");
+
+        for (Employee employee : employees){
+            if (employee.getAge() >= 40){
+                System.out.println("ФИО: " + employee.getFullName() + ", возраст: " + employee.getAge());
+            }
+        }
+
+        System.out.println("Повысим зарплату сотрудникам старше 35 лет.");
+        raiseSalary(employees);
+        System.out.println("Повысили.");
+        System.out.println();
+    }
+
+    /**
+     * Повышает зарплату всем сотрудникам старше 35 лет на 10000
+     * @param employees Массив сотрудников
+     */
+    private static void raiseSalary(Employee[] employees) {
+        for (Employee employee : employees){
+            if (employee.getAge() >= 40){
+                employee.raiseSalary(10000);
+            }
+        }
+    }
 }
